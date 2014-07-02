c579aa02430165a47f96341b795de9f10a30be7d
0xack13 <0xack13@gmail.com>
2014-07-02 12:23:11 +0300

    Initial Commit

---
+++ FSharpCookbook/Tutorial.fsx
@@ -0,0 +1,8 @@
+﻿//Using Seq
+let values = seq { 1 .. 10 }
+let doubled = Seq.map (fun v -> v * 2) values
+
+// 3 ways to print Seq variable (mainly in the interactive shell)
+printfn "%A" doubled
+printfn "%A" (Seq.toList doubled);;
+doubled;;
