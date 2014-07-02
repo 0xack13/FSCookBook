42e15963d06407b5522ad7823e59efb64dbc099e
0xack13 <0xack13@gmail.com>
2014-07-02 12:31:00 +0300

    Comment the Seq.map result

--- FSharpCookbook/FSCookBook.fsx
+++ FSharpCookbook/FSCookBook.fsx
@@ -1,6 +1,6 @@
 ﻿//Using Seq
 let values = seq { 1 .. 10 }
-let doubled = Seq.map (fun v -> v * 2) values
+let doubled = Seq.map (fun v -> v * 2) values // [2; 4; 6; 8; 10; 12; 14; 16; 18; 20]
 
 // 3 ways to print Seq variable (mainly in the interactive shell)
 printfn "%A" doubled
