3553b8f8e4806018e766452715ac32c4e59de5c8
0xack13 <0xack13@gmail.com>
2014-07-02 17:14:28 +0300

    Options Examples

--- FSharpCookbook/FSCookBook.fsx
+++ FSharpCookbook/FSCookBook.fsx
@@ -8,3 +8,22 @@ let doubled = Seq.map (fun v -> v * 2) values // [2; 4; 6; 8; 10; 12; 14; 16; 18
 printfn "%A" doubled
 printfn "%A" (Seq.toList doubled);;
 doubled;;
+
+// Using Some and None keywords
+let keepIfPositive (a : int) = if a > 0 then Some(a) else None
+keepIfPositive(-2)
+keepIfPositive(2)
+
+// Common Usage of "Option" in F#
+let rec tryFindMatch pred list =
+    match list with
+    | head :: tail -> if pred(head)
+                        then Some(head)
+                        else tryFindMatch pred tail
+    | [] -> None
+
+// result1 is Some 100 and its type is int option.
+let result1 = tryFindMatch (fun elem -> elem = 100) [ 200; 100; 50; 25 ] 
+
+// result2 is None and its type is int option.
+let result2 = tryFindMatch (fun elem -> elem = 26) [ 200; 100; 50; 25 ]
