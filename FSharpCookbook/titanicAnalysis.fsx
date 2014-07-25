e1dade91550a2f4bead486d331b6ef6c9140e3fb
0xack13 <0xack13@gmail.com>
2014-07-25 17:02:24 +0300

    Analysis of Titanic Data

---
+++ FSharpCookbook/titanicAnalysis.fsx
@@ -0,0 +1,25 @@
+﻿//To be executed in the IS
+#I "../packages/FSharp.Charting.0.90.7"
+#I "../packages/Deedle.1.0.2"
+#load "FSharp.Charting.fsx"
+#load "Deedle.fsx"
+
+open System
+open Deedle
+open FSharp.Charting
+
+
+// Create from sequence of keys and sequence of values
+let dates  = 
+  [ DateTime(2013,1,1); 
+    DateTime(2013,1,4); 
+    DateTime(2013,1,8) ]
+let values = 
+  [ 10.0; 20.0; 30.0 ]
+let first = Series(dates, values)
+
+// Create from a single list of observations
+Series.ofObservations
+  [ DateTime(2013,1,1) => 10.0
+    DateTime(2013,1,4) => 20.0
+    DateTime(2013,1,8) => 30.0 ]
