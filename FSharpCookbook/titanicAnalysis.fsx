411b9ac4eee1e6b6c7826f7b63e61c14c3ccc946
0xack13 <0xack13@gmail.com>
2014-07-26 21:28:26 +0300

    Typo in column name - case sensitive

--- FSharpCookbook/titanicAnalysis.fsx
+++ FSharpCookbook/titanicAnalysis.fsx
@@ -1,12 +1,12 @@
 ﻿//To be executed in the IS
-#I "../packages/FSharp.Charting.0.90.7"
-#I "../packages/Deedle.1.0.2"
-#I "../packages/FSharp.Data.2.0.9"
-#load "FSharp.Charting.fsx"
-#load "Deedle.fsx"
-#load "FSharp.Data.fsx"
+//#I "../packages/FSharp.Charting.0.90.7"
+//#I "../packages/Deedle.1.0.2"
+//#I "../packages/FSharp.Data.2.0.9"
+//#load "FSharp.Charting.fsx"
+//#load "Deedle.fsx"
+//#load "FSharp.Data.fsx"
 
-#r @"D:\DevCode\0xack13\FSCookbook\packages\FSharp.Data.2.0.9\lib\net40\FSharp.Data.dll"
+//#r @"D:\DevCode\0xack13\FSCookbook\packages\FSharp.Data.2.0.9\lib\net40\FSharp.Data.dll"
 
 open FSharp.Data
 open System
@@ -15,11 +15,11 @@ open FSharp.Charting
 
 // Read Titanic data & group rows by 'Sex'
 let root = "D:\\DevCode\\0xack13\\FSCookbook\\FSharpCookbook\\"
-let titanic = Frame.ReadCsv(root + "titanic.csv").GroupRowsBy<int>("Pclass")
+let titanic = Frame.ReadCsv(root + "titanic.csv").GroupRowsBy<int>("pclass")
 
 // Get 'Survived' column and count survival count per clsas
 let byClass =
-  titanic.GetColumn<bool>("Survived")
+  titanic.GetColumn<bool>("survived")
   |> Series.applyLevel fst (fun s ->
       // Get counts for 'True' and 'False' values of 'Survived'
       series (Seq.countBy id s.Values))
