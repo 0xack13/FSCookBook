c717406ac3f1fbf6d4aa5fbff5fda5f2d0b94c1e
0xack13 <0xack13@gmail.com>
2014-07-28 16:18:49 +0300

    visualize stock data

---
+++ FSharpCookbook/stockAnalysis.fsx
@@ -0,0 +1,49 @@
+﻿let msftCsv = Frame.ReadCsv(__SOURCE_DIRECTORY__ + "/data/MSFT.csv")
+let fbCsv = Frame.ReadCsv(__SOURCE_DIRECTORY__ + "/data/FB.csv")
+
+
+// Use the Date column as the index & order rows
+let msftOrd = 
+  msftCsv
+  |> Frame.indexRowsDate "Date"
+  |> Frame.sortRowsByKey
+
+
+// Create data frame with just Open and Close prices
+let msft = msftOrd.Columns.[ ["Open"; "Close"] ]
+
+// Add new column with the difference between Open & Close
+msft?Difference <- msft?Open - msft?Close
+
+// Do the same thing for Facebook
+let fb = 
+  fbCsv
+  |> Frame.indexRowsDate "Date"
+  |> Frame.sortRowsByKey
+  |> Frame.sliceCols ["Open"; "Close"]
+fb?Difference <- fb?Open - fb?Close
+
+// Now we can easily plot the differences
+Chart.Combine
+  [ Chart.Line(msft?Difference |> Series.observations) 
+    Chart.Line(fb?Difference |> Series.observations) ]
+
+
+
+// Change the column names so that they are unique
+let msftNames = ["MsftOpen"; "MsftClose"; "MsftDiff"]
+let msftRen = msft |> Frame.indexColsWith msftNames
+
+let fbNames = ["FbOpen"; "FbClose"; "FbDiff"]
+let fbRen = fb |> Frame.indexColsWith fbNames
+
+// Outer join (align & fill with missing values)
+let joinedOut = msftRen.Join(fbRen, kind=JoinKind.Outer)
+
+// Inner join (remove rows with missing values)
+let joinedIn = msftRen.Join(fbRen, kind=JoinKind.Inner)
+
+// Visualize daily differences on available values only
+Chart.Rows
+  [ Chart.Line(joinedIn?MsftDiff |> Series.observations) 
+    Chart.Line(joinedIn?FbDiff |> Series.observations) ]
