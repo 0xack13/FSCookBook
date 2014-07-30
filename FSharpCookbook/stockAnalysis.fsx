7967f8e14f7908cef628e0250831ae3cb628c12a
0xack13 <0xack13@gmail.com>
2014-07-30 16:13:14 +0300

    Grouping and projection for dare/time group

--- FSharpCookbook/stockAnalysis.fsx
+++ FSharpCookbook/stockAnalysis.fsx
@@ -64,3 +64,56 @@ let jan = joinedIn.Rows.[DateTime(2013, 1, 1) .. DateTime(2013, 1, 31)]
 // Calculate means over the period
 jan?FbOpen |> Stats.mean
 jan?MsftOpen |> Stats.mean
+
+
+
+
+// Create from sequence of keys and sequence of values
+let dates  = 
+  [ DateTime(2013,1,1); 
+    DateTime(2013,1,4); 
+    DateTime(2013,1,8) ]
+let values = 
+  [ 10.0; 20.0; 30.0 ]
+let first = Series(dates, values)
+
+// Create from a single list of observations
+Series.ofObservations
+  [ DateTime(2013,1,1) => 10.0
+    DateTime(2013,1,4) => 20.0
+    DateTime(2013,1,8) => 30.0 ]
+
+// Shorter alternative to 'Series.ofObservations'
+series [ 1 => 1.0; 2 => 2.0 ]
+
+// Create series with implicit (ordinal) keys
+Series.ofValues [ 10.0; 20.0; 30.0 ]
+
+/// Generate date range from 'first' with 'count' days
+let dateRange (first:System.DateTime) count = (*[omit:(...)]*)
+  seq { for i in 0 .. (count - 1) -> first.AddDays(float i) }(*[/omit]*)
+
+/// Generate 'count' number of random doubles
+let rand count = (*[omit:(...)]*)
+  let rnd = System.Random()
+  seq { for i in 0 .. (count - 1) -> rnd.NextDouble() }(*[/omit]*)
+
+// A series with values for 10 days 
+let second = Series(dateRange (DateTime(2013,1,1)) 10, rand 10)
+
+
+// A series with values for 10 days 
+let second = Series(dateRange (DateTime(2013,1,1)) 10, rand 10)
+
+let daysSeries = Series(dateRange DateTime.Today 10, rand 10)
+let obsSeries = Series(dateRange DateTime.Now 10, rand 10)
+
+let daysFrame = [ 1 => daysSeries ] |> Frame.ofColumns
+let obsFrame = [ 2 => obsSeries ] |> Frame.ofColumns
+
+//Grouping and aggregation
+let monthly =
+  joinedIn
+  |> Frame.groupRowsUsing (fun k _ -> DateTime(k.Year, k.Month, 1))
+//Get specified rows in the group and calculate the mean
+monthly.Rows.[DateTime(2013,5,1), *] |> Stats.mean
