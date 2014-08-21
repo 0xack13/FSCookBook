84e10ceaabe217c55db8d9bc4656116b69ccfa16
0xack13 <0xack13@gmail.com>
2014-08-21 15:44:53 +0300

    Add data structure script

---
+++ FSharpCookbook/DataStructure.fsx
@@ -0,0 +1,8 @@
+﻿type Tree<'a> = 
+    | Node of (*data*)‘a * (*left*)Tree<‘a> * (*right*)Tree<‘a> 
+    | Leaf
+//     4 
+//  2     6 
+// 1 3   5 7 
+let tree7 = Node(4, Node(2, Node(1, Leaf, Leaf), Node(3, Leaf, Leaf)), 
+                    Node(6, Node(5, Leaf, Leaf), Node(7, Leaf, Leaf)))
