101861e3384495699466cce7ea7e36ef3b79ca6c
0xack13 <0xack13@gmail.com>
2014-08-27 12:15:45 +0300

    Add recursive function for the song iteration

--- FSharpCookbook/Algorithms/99beers.fsx
+++ FSharpCookbook/Algorithms/99beers.fsx
@@ -1,1 +1,15 @@
 ﻿
+#light
+let rec bottles n =
+    let (before, after) = match n with
+                          | 1 -> ("bottle", "bottles")
+                          | 2 -> ("bottles", "bottle")
+                          | n -> ("bottles", "bottles")
+    printfn "%d %s of beer on the wall" n before
+    printfn "%d %s of beer" n before
+    printfn "Take one down, pass it around"
+    printfn "%d %s of beer on the wall\n" (n - 1) after
+    if n > 1 then
+        bottles (n - 1)
+// Run on the F# Interactive Shell: 
+// > bottles 9;;
