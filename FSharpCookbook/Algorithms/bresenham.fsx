4d33561126cc0494ccac982410efcd1a00ae7137
0xack13 <0xack13@gmail.com>
2014-09-01 11:51:00 +0300

    WordLadder in F#

---
+++ FSharpCookbook/Algorithms/bresenham.fsx
@@ -0,0 +1,39 @@
+﻿
+let inline bresenham fill (x0, y0) (x1, y1) =
+  let steep = abs(y1 - y0) > abs(x1 - x0)
+  let x0, y0, x1, y1 =
+    if steep then y0, x0, y1, x1 else x0, y0, x1, y1
+  let x0, y0, x1, y1 =
+    if x0 > x1 then x1, y1, x0, y0 else x0, y0, x1, y1
+  let dx, dy = x1 - x0, abs(y1 - y0)
+  let s = if y0 < y1 then 1 else -1
+  let rec loop e x y =
+    if x <= x1 then
+      if steep then fill y x else fill x y
+      if e < dy then
+        loop (e-dy+dx) (x+1) (y+s)
+      else
+        loop (e-dy) (x+1) y
+  loop (dx/2) x0 y0
+
+
+#r @"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5\PresentationFramework.dll"
+open System
+open System.Windows
+open System.Windows.Media.Imaging
+
+ 
+[<System.STAThread>]
+do
+  let rand = System.Random()
+  let n = 256
+  let pixel = Array.create (n*n) 0uy
+  let rand = System.Random().Next
+  for _ in 1..100 do
+    bresenham (fun x y -> pixel.[x+y*n] <- 255uy) (rand n, rand n) (rand n, rand n)
+  let image = Controls.Image(Stretch=Media.Stretch.Uniform)
+  let format = Media.PixelFormats.Gray8
+  image.Source <-
+    BitmapSource.Create(n, n, 1.0, 1.0, format, null, pixel, n)
+  Window(Content=image, Title="Bresenham's line algorithm")
+  |> (Application()).Run |> ignore
---
+++ FSharpCookbook/Algorithms/wordladder.fsx
@@ -0,0 +1,65 @@
+﻿let root = __SOURCE_DIRECTORY__ + "\\"
+let words = System.IO.File.ReadAllLines(root + "short.txt")
+ 
+let isWordInDict word =
+  words |> Seq.exists (fun w -> w = word)
+ 
+let filterToDict words =
+  words |> List.filter isWordInDict
+ 
+let filterNotSeen (seen : Set<string>) words =
+  words |> List.filter (fun w -> not(seen.Contains(w)))
+ 
+let findChildren (word : string) = [
+  let generateCandidates prefix char postfix =
+    ['a'..'z'] |> List.filter (fun i -> i <> char)
+      |> List.map (fun l -> prefix + l.ToString() + postfix)
+ 
+  yield! generateCandidates "" (word.[0]) (word.[1..])
+  for i in 1..3 do
+    yield! generateCandidates (word.[0..(i-1)]) (word.[i]) (word.[(i+1)..])
+]
+ 
+let findValidUniqueChildren seen = findChildren >> filterNotSeen seen >> filterToDict
+ 
+type node = string List * string // parent List * word
+ 
+let rec buildNodes (words : string List) (parents : string List) (nodes : node List) = 
+  match words with
+    | [] -> nodes
+    | w :: tail -> buildNodes tail parents ((parents, w) :: nodes)
+ 
+let rec findLadderWorker (searchNodes : node List) (endword : string) (seen : Set<string>) =
+  let queuechildren word parents searchNodes =
+    let children = findValidUniqueChildren seen word
+    let newparents = word :: parents
+    let childnodes = buildNodes children newparents []
+    let newSearchNodes = searchNodes @ childnodes
+    findLadderWorker newSearchNodes endword (seen + (Set.ofList children))
+    
+  let testnode (node : node) (searchNodes : node List) =
+    match node with
+      | parents, word when word = endword -> word :: parents |> List.rev
+      | parents, word -> queuechildren word parents searchNodes
+ 
+  match searchNodes with
+    | [] -> []
+    | node :: others -> testnode node others
+  
+let findLadder startword endword =
+  if startword = endword then
+    [endword]
+  else
+    let children = findValidUniqueChildren Set.empty startword
+    let seen = Set.ofList (startword :: children)
+    let startNodes = buildNodes children [startword] []
+    findLadderWorker startNodes endword seen
+ 
+let args = System.Environment.GetCommandLineArgs()
+if args.Length < 3 then // 3 is a hack to support both fsi and fsc
+  printfn "You forgot to include start and end word arguments"
+else
+  let startword = args.[(args.Length - 2)]
+  let endword = args.[(args.Length - 1)]
+  let ladder = findLadder startword endword
+  printfn "%A" ladder
