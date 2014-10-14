77ea1061d32bfbad6d43413e39b7251300dedcf9
0xack13 <0xack13@gmail.com>
2014-10-15 02:47:45 +0300

    Reflection  for tuple values

---
+++ FSharpCookbook/FSIntroSpection.fsx
@@ -0,0 +1,13 @@
+﻿open Microsoft.FSharp.Reflection
+
+let printTupleValues x =
+    if FSharpType.IsTuple( x.GetType() ) then
+        let s =
+            FSharpValue.GetTupleFields( x )
+            |> Array.map (fun a -> a.ToString())
+            |> Array.reduce (fun a b -> sprintf "%s, %s" a b)
+        printfn "(%s)" s
+    else 
+        printfn "not a tuple"
+
+//printTupleValues ("hello world", 1)
