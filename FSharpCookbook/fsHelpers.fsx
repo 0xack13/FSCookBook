74efc4e61792686b8be5f23dc8dba9dce5f63920
Saleh Batati <0xack13@gmail.com>
2014-10-22 17:00:48 +0300

    F# Moving Average

--- FSharpCookbook/fsHelpers.fsx
+++ FSharpCookbook/fsHelpers.fsx
@@ -90,3 +90,19 @@ let myArray  = Array.init 100 (fun x -> {value1 = "x"; value2 = "y"})
 
 //using Generics
 //let myArray  = Array.init<record1> 100 (fun x -> {value1 = "x"; value2 = "y"})
+
+//Moving Average
+let sma period f (list:float list) =
+    let sma_aux queue v =
+        let q = Seq.truncate period (v :: queue)
+        Seq.average q, Seq.toList q
+    List.fold (fun s v ->
+        let avg,state = sma_aux s v
+        f avg
+        state) [] list
+ 
+printf "sma3: "
+[ 1.;2.;3.;4.;5.;5.;4.;3.;2.;1.] |> sma 3 (printf "%.2f ")
+printf "\nsma5: "
+[ 1.;2.;3.;4.;5.;5.;4.;3.;2.;1.] |> sma 5 (printf "%.2f ")
+printfn ""
