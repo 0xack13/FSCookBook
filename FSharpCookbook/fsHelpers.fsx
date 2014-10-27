887fad9bf4bc812c1e7f1ffb06392345a0c10767
0xack13 <0xack13@gmail.com>
2014-10-28 02:10:51 +0300

    Use take and truncate

--- FSharpCookbook/fsHelpers.fsx
+++ FSharpCookbook/fsHelpers.fsx
@@ -90,3 +90,25 @@ let myArray  = Array.init 100 (fun x -> {value1 = "x"; value2 = "y"})
 
 //using Generics
 //let myArray  = Array.init<record1> 100 (fun x -> {value1 = "x"; value2 = "y"})
+
+//Using Take and use full sequence printing
+let list = [ 1 .. 10 ]
+let res = list |> Seq.take 5
+printf "Res Sequence: %A" (res |> Seq.toList)
+
+let mySeq = seq { for i in 1 .. 10 -> i*i }
+let truncatedSeq = Seq.truncate 5 mySeq
+let takenSeq = Seq.take 5 mySeq
+
+let truncatedSeq2 = Seq.truncate 20 mySeq
+let takenSeq2 = Seq.take 20 mySeq
+
+let printSeq seq1 = Seq.iter (printf "%A ") seq1; printfn "" 
+
+// Up to this point, the sequences are not evaluated. 
+// The following code causes the sequences to be evaluated.
+truncatedSeq |> printSeq
+truncatedSeq2 |> printSeq
+takenSeq |> printSeq
+// The following line produces a run-time error (in printSeq):
+takenSeq2 |> printSeq
