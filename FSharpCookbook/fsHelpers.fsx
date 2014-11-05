60ee029aff0c44682de482676bf425659b7c8561
0xack13 <0xack13@gmail.com>
2014-11-05 11:42:15 +0300

    Using Active Patterns

--- FSharpCookbook/fsHelpers.fsx
+++ FSharpCookbook/fsHelpers.fsx
@@ -217,3 +217,15 @@ let mapReduce xs =
     map xs
     |> List.sort
     |> reduce
+
+//Active Patterns
+let (|Even|Odd|) input = if input % 2 = 0 then Even else Odd
+
+let TestNumber input =
+   match input with
+   | Even -> printfn "%d is even" input
+   | Odd -> printfn "%d is odd" input
+
+TestNumber 7
+TestNumber 11
+TestNumber 32
